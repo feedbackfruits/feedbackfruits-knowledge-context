ad8b44bcc122ac6dc12b80bbe5b491456406bede
Steffan Sluis <steffansluis@gmail.com>
2017-07-06 22:03:41 +0200

    Add tslint

--- dist/khan_academy.js
+++ dist/khan_academy.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Topic = '<https://www.khanacademy.org/Topic>';
-exports.childTopic = '<https://www.khanacademy.org/childTopic>';
-exports.parentTopic = '<https://www.khanacademy.org/parentTopic>';
+exports.Topic = "<https://www.khanacademy.org/Topic>";
+exports.childTopic = "<https://www.khanacademy.org/childTopic>";
+exports.parentTopic = "<https://www.khanacademy.org/parentTopic>";
--- src/mag.ts
+++ src/mag.ts
@@ -1,3 +1,3 @@
-export const FieldOfStudy = '<http://academic.microsoft.com/FieldOfStudy>';
-export const parentFieldOfStudy = '<http://academic.microsoft.com/parentFieldOfStudy>';
-export const childFieldOfStudy = '<http://academic.microsoft.com/childFieldOfStudy>';
+export const FieldOfStudy = "<http://academic.microsoft.com/FieldOfStudy>";
+export const parentFieldOfStudy = "<http://academic.microsoft.com/parentFieldOfStudy>";
+export const childFieldOfStudy = "<http://academic.microsoft.com/childFieldOfStudy>";
