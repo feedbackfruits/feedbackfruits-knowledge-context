8a795df825260e1b5094f4ba60c5ce2a9465baca
Steffan Sluis <steffansluis@gmail.com>
2017-07-27 15:48:46 +0200

    Update tslint and add missing predicates

--- src/khan_academy.ts
+++ src/khan_academy.ts
@@ -1,3 +1,3 @@
-export const Topic = "<https://www.khanacademy.org/Topic>";
-export const childTopic = "<https://www.khanacademy.org/childTopic>";
-export const parentTopic = "<https://www.khanacademy.org/parentTopic>";
+export const Topic = '<https://www.khanacademy.org/Topic>';
+export const childTopic = '<https://www.khanacademy.org/childTopic>';
+export const parentTopic = '<https://www.khanacademy.org/parentTopic>';
