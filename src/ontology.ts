753311ec5775b8ed6d63f3fb29423dbd3891f3c5
Joost Verdoorn <jpverdoorn@gmail.com>
2018-01-02 22:19:21 +0100

    Add ontology types

---
+++ src/ontology.ts
@@ -0,0 +1,99 @@
+
+export module prefixes {
+    export const rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+    export const rdfs = "http://www.w3.org/2000/01/rdf-schema#";
+    export const xsd = "http://www.w3.org/2001/XMLSchema#";
+    export const owl = "http://www.w3.org/2002/07/owl#";
+    export const skos = "http://www.w3.org/2004/02/skos/core#";
+    export const schema = "http://schema.org/";
+    export const mag = "http://academic.microsoft.com/";
+    export const oa = "http://www.w3.org/ns/oa#";
+    export const $ = "https://knowledge.express/";
+}
+export module graph {
+    export module $ {
+        export const Annotation = "https://knowledge.express/Annotation";
+        export const Tag = "https://knowledge.express/Tag";
+        export const startPosition = "https://knowledge.express/startPosition";
+        export const confidence = "https://knowledge.express/confidence";
+        export const detectedAs = "https://knowledge.express/detectedAs";
+        export const Entity = "https://knowledge.express/Entity";
+        export const Document = "https://knowledge.express/Document";
+        export const Resource = "https://knowledge.express/Resource";
+        export const Taggable = "https://knowledge.express/Taggable";
+        export const section = "https://knowledge.express/section";
+        export const DocumentSection = "https://knowledge.express/DocumentSection";
+        export const sectionOf = "https://knowledge.express/sectionOf";
+        export const endPosition = "https://knowledge.express/endPosition";
+        export const topic = "https://knowledge.express/topic";
+        export const Topic = "https://knowledge.express/Topic";
+        export const resource = "https://knowledge.express/resource";
+        export const tag = "https://knowledge.express/tag";
+        export const annotation = "https://knowledge.express/annotation";
+        export const about = "https://knowledge.express/about";
+        export const subjectOf = "https://knowledge.express/subjectOf";
+        export const Video = "https://knowledge.express/Video";
+        export const caption = "https://knowledge.express/caption";
+        export const VideoCaption = "https://knowledge.express/VideoCaption";
+        export const captionOf = "https://knowledge.express/captionOf";
+        export const startsAfter = "https://knowledge.express/startsAfter";
+        export const duration = "https://knowledge.express/duration";
+        export const fieldOfStudy = "https://knowledge.express/fieldOfStudy";
+        export const entity = "https://knowledge.express/entity";
+        export const document = "https://knowledge.express/document";
+        export const score = "https://knowledge.express/score";
+        export const previous = "https://knowledge.express/previous";
+        export const next = "https://knowledge.express/next";
+        export const parent = "https://knowledge.express/parent";
+        export const child = "https://knowledge.express/child";
+    }
+    export module rdf {
+        export const type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+    }
+    export module owl {
+        export const Class = "http://www.w3.org/2002/07/owl#Class";
+        export const DatatypeProperty = "http://www.w3.org/2002/07/owl#DatatypeProperty";
+        export const Thing = "http://www.w3.org/2002/07/owl#Thing";
+        export const ObjectProperty = "http://www.w3.org/2002/07/owl#ObjectProperty";
+        export const FunctionalProperty = "http://www.w3.org/2002/07/owl#FunctionalProperty";
+        export const inverseOf = "http://www.w3.org/2002/07/owl#inverseOf";
+        export const sameAs = "http://www.w3.org/2002/07/owl#sameAs";
+    }
+    export module rdfs {
+        export const subClassOf = "http://www.w3.org/2000/01/rdf-schema#subClassOf";
+        export const domain = "http://www.w3.org/2000/01/rdf-schema#domain";
+        export const range = "http://www.w3.org/2000/01/rdf-schema#range";
+        export const subPropertyOf = "http://www.w3.org/2000/01/rdf-schema#subPropertyOf";
+    }
+    export module xsd {
+        export const integer = "http://www.w3.org/2001/XMLSchema#integer";
+        export const decimal = "http://www.w3.org/2001/XMLSchema#decimal";
+        export const string = "http://www.w3.org/2001/XMLSchema#string";
+        export const duration = "http://www.w3.org/2001/XMLSchema#duration";
+    }
+    export module schema {
+        export const CreativeWork = "http://schema.org/CreativeWork";
+        export const name = "http://schema.org/name";
+        export const description = "http://schema.org/description";
+        export const image = "http://schema.org/image";
+        export const license = "http://schema.org/license";
+        export const sourceOrganization = "http://schema.org/sourceOrganization";
+        export const text = "http://schema.org/text";
+        export const about = "http://schema.org/about";
+        export const subjectOf = "http://schema.org/subjectOf";
+        export const VideoObject = "http://schema.org/VideoObject";
+    }
+    export module mag {
+        export const FieldOfStudy = "http://academic.microsoft.com/FieldOfStudy";
+        export const parentFieldOfStudy = "http://academic.microsoft.com/parentFieldOfStudy";
+        export const childFieldOfStudy = "http://academic.microsoft.com/childFieldOfStudy";
+    }
+    export module oa {
+        export const SemanticTag = "http://www.w3.org/ns/oa#SemanticTag";
+    }
+}
+
+export default {
+    prefixes,
+    graph
+}
