ffc27a6376f518f628eae8522a53d225143f4788
Steffan Sluis <steffansluis@gmail.com>
2018-02-02 01:19:37 +0100

    Cleanup types/natives and owl:FunctionalProperty

--- src/typescript/prefixes.ts
+++ src/typescript/prefixes.ts
@@ -4,9 +4,9 @@ export module prefixes {
     export const rdfs = "http://www.w3.org/2000/01/rdf-schema#";
     export const xsd = "http://www.w3.org/2001/XMLSchema#";
     export const owl = "http://www.w3.org/2002/07/owl#";
-    export const skos = "http://www.w3.org/2004/02/skos/core#";
     export const schema = "http://schema.org/";
     export const mag = "http://academic.microsoft.com/";
     export const oa = "http://www.w3.org/ns/oa#";
+    export const cc = "http://creativecommons.org/ns#";
     export const $ = "https://knowledge.express/";
 };
